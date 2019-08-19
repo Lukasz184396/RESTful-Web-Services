c46fc02ee389e22e7839bd6540a4cb614836cc41
Lukasz184396 <zawadalukasz88@gmail.com>
2019-08-20 00:23:31 +0200

    Revert "Step 10 - Implementing Generic Exception Handling for all Resources"
    This reverts commit bac613e3dc21d3cc9cb40fd809b65ffcabb81baf.

--- src/main/java/com/zawadal/restfulwebservice/user/UserResource.java
+++ src/main/java/com/zawadal/restfulwebservice/user/UserResource.java
@@ -23,7 +23,7 @@ public class UserResource {
     public User reytiveUser(@PathVariable int id) {
         User user = service.findOne(id);
         if(user == null)
-            throw new UserNotFoundException("id-" + id);
+            throw new UsrNotFoundException("id-" + id);
         return user;
     }
 
